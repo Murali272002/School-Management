44fc36a0e8d21dcee9c938ed7304a9b0e87a78b2
Murali272002 <muralidharan27082002@gmail.com>
2024-06-23 16:23:05 +0530

    FEAT : Added API for Fetching Teachers from Database

--- src/main/java/com/essilor/test/Testing/controller/TeacherController.java
+++ src/main/java/com/essilor/test/Testing/controller/TeacherController.java
@@ -5,6 +5,8 @@ import com.essilor.test.Testing.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/school-management/api/v1")
 public class TeacherController {
@@ -12,13 +14,13 @@ public class TeacherController {
     @Autowired
     TeacherService teacherService;
 
-    @GetMapping("/getName")
-    public String getName(){
-        return "Muralidharan Narayana";
-    }
-
     @PostMapping("/teacher")
     public Teacher addTeacher(@RequestBody Teacher teacher){
         return teacherService.addTeacher(teacher);
+    }
+
+    @GetMapping("/teachers")
+    public List<Teacher> getTeachers(){
+        return teacherService.getTeachers();
     }
 }
